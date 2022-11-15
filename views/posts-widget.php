72e21652331721c85e0495a594e15b0bda64ff2d
Jhed Mendoza <jhed.mendoza@hotmail.com>
2022-11-15 20:29:00 +0800

    Removed custom title

--- views/posts-widget.php
+++ views/posts-widget.php
@@ -20,8 +20,8 @@
 ?>
 
 <div class="fc-posts-widget <?php echo $widget_class; ?>">
-<?php 
-	foreach( $include_posts as $posts ) { // start loop - posts 
+<?php
+	foreach( $include_posts as $posts ) { // start loop - posts
 
 		// get included post
 		$post   = get_post( $posts['included_post_id'] );
@@ -29,11 +29,12 @@
 		// custom content
 		$custom_title   = $posts['post_custom_title'];
 		$custom_content = $posts['post_custom_text'];
-		
+
 		// post meta
 		$user_id   		   = get_userdata($post->post_author)->data->ID;
 		$post_url          = get_permalink($post);
-		$post_title        = $custom_title ?: $post->post_title;
+		// $post_title        = $custom_title ?: $post->post_title;
+		$post_title        = $post->post_title;
 		$post_excerpt      = get_the_excerpt($post);
 		$post_author       = get_userdata($post->post_author)->display_name;
 		$post_author_title = isset(get_user_meta($user_id)['agent_title'][0]) ? get_user_meta($user_id)['agent_title'][0] : '';
@@ -41,7 +42,7 @@
 ?>
 		<div class="fc-post <?php echo $layout_class; ?>">
 
-			
+
 			<?php if( $title_above ) { ?>
 				<h3 class="fc-post-title">
 					<a href="<?php echo esc_url($post_url); ?>" class="fc-post-title-link"><?php echo $post_title; ?></a>
@@ -49,11 +50,11 @@
 			<?php } ?>
 			<div class="fc-post-image">
 				<a href="<?php echo esc_url($post_url); ?>" class="fc-post-image-link">
-					<?php 
+					<?php
 						// has featured image
 						if( has_post_thumbnail($post) ) { ?>
 						<img src="<?php echo esc_url( get_the_post_thumbnail_url( $post->ID, 'post-thumbnail') ); ?>" alt="">
-					<?php } 
+					<?php }
 						// no featured image, show placeholder
 						else { ?>
 						<img src="<?php echo esc_url( plugins_url( self::$slug . '/assets/img/placeholder.jpg') ); ?>" alt="">
@@ -111,7 +112,7 @@
 			</div>
 
 		</div>
-<?php } // end loop - posts 
+<?php } // end loop - posts
 ?>
 </div>
 <?php
